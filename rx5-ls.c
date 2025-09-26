395132798833050917899037f09e3ea65321915d
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-09-26 17:28:04 +0200

    Add loop parameters

--- rx5-ls.c
+++ rx5-ls.c
@@ -4,6 +4,9 @@
 #include <stdio.h>
 #include <string.h>
 uint8_t rom[128 * 1024];
+int32_t int24(uint8_t *p) {
+  return (((int32_t)p[0] & 1) << 16) | ((int32_t)p[1] << 8) | (int32_t)p[2];
+}
 int main(void) {
   int i, nvoices;
   if (fread(rom, 1, sizeof(rom), stdin) != sizeof(rom))
@@ -17,14 +20,18 @@ int main(void) {
   printf("Number of voices: %d\n", nvoices);
   for (i = 0; i < nvoices; i++) {
     uint8_t *p = rom + 6 + 32 * i;
-    int pcmstart;
+    int32_t pcmstart, loopstart, loopend;
     puts("---");
     printf("Voice name: %6.6s\n", p + 26);
     printf("Pitch: octave %d note %d\n", p[0], p[1]);
     printf("PCM format: %s\n", p[2] ? "12-bit" : "8-bit");
-    printf("Loop: %s\n", p[3] & 0x40 ? "yes" : "no");
+    printf("Loop: %s\n", !(p[3] & 0x40) ? "yes" : "no");
     pcmstart = ((int)(p[3] & 1) << 16) | ((int)(p[4]) << 8);
     printf("PCM data start: %#x\n", pcmstart);
+    loopstart = int24(p + 5);
+    printf("Loop start: %#x\n", loopstart);
+    loopend = int24(p + 8);
+    printf("Loop end: %#x\n", loopend);
   }
   return 0;
 }
