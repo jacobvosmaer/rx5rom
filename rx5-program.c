0a7a5062670c52513261e16dc9d22f0de6e1c057
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-10-02 22:53:06 +0200

    rx5-program: still not working correctly

--- rx5-program.c
+++ rx5-program.c
@@ -5,16 +5,24 @@
 #include <stdio.h>
 #include <string.h>
 #define MSGSIZE 64
-#define TIMEOUT 1000
-uint8_t request[MSGSIZE + 1] = {0, 'R', 'X', '5', 1, '?', '?', 0,
-                                0, 0,   0,   0,   0, 2,   0},
+uint8_t request[MSGSIZE + 1] = {0, 'R', 'X', '5', 1, '?', '?'},
                           response[MSGSIZE];
 int main(void) {
   int i;
-  hid_device *dev;
+  hid_device *dev = 0;
+  struct hid_device_info *devinfo = hid_enumerate(0x6112, 0x5550);
+  uint32_t size = 2 * 128 * 1024;
+  for (i = 0; i < 4; i++)
+    request[11 + i] = size >> (i * 8);
+  for (; devinfo; devinfo = devinfo->next) {
+    if (devinfo->usage_page == 0xffab && devinfo->usage == 0x200) {
+      dev = hid_open_path(devinfo->path);
+      break;
+    }
+  }
+  if (!dev)
+    errx(-1, "failed to open usb device");
   fputs("open device: ", stderr);
-  while (dev = hid_open(0x6112, 0x5550, 0), !dev)
-    ;
   fputs("ok\n", stderr);
   fputs("write handshake: ", stderr);
   while (hid_write(dev, request, sizeof(request)) != sizeof(request))
@@ -28,21 +36,22 @@ int main(void) {
   request[14] = 'K';
   if (memcmp(request + 1, response, MSGSIZE))
     errx(-1, "invalid handshake response");
-
-  for (i = 0; i < 2048; i++) {
+  fputs("write flash: ", stderr);
+  for (i = 0; i < size / 64; i++) {
     if (fread(request + 1, 1, MSGSIZE, stdin) != MSGSIZE)
       errx(-1, "short read from stdin");
-    fputc('.', stderr);
     while (hid_write(dev, request, sizeof(request)) != sizeof(request))
       ;
-    fputc('o', stderr);
     if (0) {
       while (hid_read(dev, response, sizeof(response)) != sizeof(response))
         ;
-      fputc('O', stderr);
       if (memcmp(request + 1, response, MSGSIZE))
-        errx(-1, "I/O error: invalid response from device");
+        errx(-1, "read back of block %d failed", i);
     }
+    if (i > 0 && !(i % 64))
+      fputc('.', stderr);
   }
+  fputs(" ok\n", stderr);
+  hid_close(dev);
   return 0;
 }
