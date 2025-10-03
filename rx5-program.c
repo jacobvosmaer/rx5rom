d485cccb9c47d3b2298df0f9604acf8b56c57556
Jacob Vosmaer <contact@jacobvosmaer.nl>
2025-10-03 08:13:01 +0200

    rx5-program: still not working

--- rx5-program.c
+++ rx5-program.c
@@ -10,11 +10,13 @@ uint8_t request[MSGSIZE + 1] = {0, 'R', 'X', '5', 1, '?', '?'},
 int main(void) {
   int i;
   hid_device *dev = 0;
-  struct hid_device_info *devinfo = hid_enumerate(0x6112, 0x5550);
+  struct hid_device_info *devinfo;
   uint32_t size = 2 * 128 * 1024;
   for (i = 0; i < 4; i++)
     request[11 + i] = size >> (i * 8);
-  for (; devinfo; devinfo = devinfo->next) {
+  fputs("open device: ", stderr);
+  for (devinfo = hid_enumerate(0x6112, 0x5550); devinfo;
+       devinfo = devinfo->next) {
     if (devinfo->usage_page == 0xffab && devinfo->usage == 0x200) {
       dev = hid_open_path(devinfo->path);
       break;
@@ -22,7 +24,6 @@ int main(void) {
   }
   if (!dev)
     errx(-1, "failed to open usb device");
-  fputs("open device: ", stderr);
   fputs("ok\n", stderr);
   fputs("write handshake: ", stderr);
   while (hid_write(dev, request, sizeof(request)) != sizeof(request))
@@ -42,8 +43,9 @@ int main(void) {
       errx(-1, "short read from stdin");
     while (hid_write(dev, request, sizeof(request)) != sizeof(request))
       ;
-    if (0) {
-      while (hid_read(dev, response, sizeof(response)) != sizeof(response))
+    if (10) {
+      while (hid_read_timeout(dev, response, sizeof(response), 1000) !=
+             sizeof(response))
         ;
       if (memcmp(request + 1, response, MSGSIZE))
         errx(-1, "read back of block %d failed", i);
